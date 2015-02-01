9b89e057dd06905352be793d9d9c7d63d4e1274f
Matthias Schuetz <mschuetz@gmail.com>
2015-02-01 01:45:38 +0100

    implemented converting parse tree to map

--- src/friedrich/lua_notation.clj
+++ src/friedrich/lua_notation.clj
@@ -1,6 +1,7 @@
 (ns friedrich.lua-notation
   (:require [instaparse.core :as i]
-            [instaparse.combinators :as c]))
+            [instaparse.combinators :as c])
+  (:import (sun.reflect.generics.reflectiveObjects NotImplementedException)))
 
 (def lua-grammar "
   Program = (SPACE* Assignment)* SPACE*
@@ -10,10 +11,10 @@
   VarName = #'[a-zA-Z][a-zA-Z0-9]+'
 
   Map = <#'\\{\\s*'> Pairs <#'\\s*\\}'>
-  Pairs = Pair (<#'\\s*,\\s*'> Pair)* | ε
+  <Pairs> = Pair (<#'\\s*,\\s*'> Pair)* | ε
   Pair = PairKey <#'\\s*=\\s*'> PairValue
-  PairKey = <#'\\[\\s*'> (Value) <#'\\s*\\]'>
-  PairValue = Value
+  <PairKey> = <#'\\[\\s*'> (Value) <#'\\s*\\]'>
+  <PairValue> = Value
 
   String = <'\"'> #'[^\\\"]*' <'\"'>
   Number = #'[0-9]+'
@@ -23,3 +24,44 @@
 ")
 
 (def parse (i/parser lua-grammar))
+
+(defn- parse-varname [[key value]]
+  (assert (= :VarName key))
+  (keyword value))
+
+; TODO try to parse as double, bigint, etc. if it fails
+(defn- parse-number [strvalue]
+  (Long/parseLong strvalue))
+
+(declare parse-value)
+
+(defn- parse-pair [[type key value]]
+  (assert (= :Pair type))
+  [(parse-value key) (parse-value value)])
+
+(defn- parse-map [pairs]
+  (if (nil? pairs)
+    {}
+    (into {}
+          (map parse-pair pairs))))
+
+(defn- parse-value [[type & values]]
+  (case type
+    :String (first values)
+    :Bool (Boolean/parseBoolean (first values))
+    :Number (parse-number (first values))
+    :Map (parse-map values)))
+
+(defn- parse-assignment [[key & rest]]
+  (assert (= :Assignment key))
+  [(parse-varname (first rest))
+   (parse-value (second rest))])
+
+(defn- parse-program [[key & assignments]]
+  (assert (= :Program key))
+  (map parse-assignment assignments))
+
+(defn as-map [node]
+  (case (first node)
+    :Program (into {} (parse-program node))
+    (throw (Exception. "expected :Program"))))
--- test/friedrich/lua_notation_test.clj
+++ test/friedrich/lua_notation_test.clj
@@ -1,1 +1,44 @@
-(ns friedrich.lua-notation-test)
+(ns friedrich.lua-notation-test
+  (:require [clojure.test :refer :all]
+            [friedrich.lua-notation :refer :all]
+            ))
+
+(def testinput
+  "
+  bar = {
+     [\"UID\"] = \"landevice4229\",
+     [\"_node\"] = \"landevice0\",
+     [\"active\"] = false
+  }
+  baz = {}
+  foo = true
+  quux = 123
+  ")
+
+;(deftest perf-test
+;  (time (parse (slurp (str (System/getenv "HOME") "/test.lua"))))
+;  (time (parse testinput)))
+
+(def very-simple "
+foo = \"bar\"
+baz = 123
+quux = false
+")
+
+(deftest test-as-map-assignments
+  (testing ""
+    (is (= (as-map (parse very-simple)) {:foo "bar" :baz 123 :quux false}))))
+
+(def with-map "
+foo = \"bar\"
+baz = {
+  [\"quux\"] = false,
+  [\"1\"] = {
+    [\"2\"] = 3
+  }
+}
+")
+
+(deftest test-as-map-with-map
+  (testing ""
+    (is (= (as-map (parse with-map)) {:foo "bar" :baz {"quux" false "1" {"2" 3}}}))))
